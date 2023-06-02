4bb7515f77d5595d44e812295a3c308b86ac89ec
surajverma2587 <surajverma2587@gmail.com>
2023-06-02 18:57:17 +0100

    complete WordCounter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { WordCounter } from "./components/WordCounter";
 
 export const App = () => {
   return (
     <div>
       <Header title="Word Counter" subTitle="Paste or type your text here" />
+      <WordCounter />
     </div>
   );
 };
---
+++ src/components/WordCounter.js
@@ -0,0 +1,13 @@
+export const WordCounter = () => {
+  return (
+    <div>
+      <div>
+        <textarea
+          rows={10}
+          placeholder="Type or paste your text here..."
+        ></textarea>
+      </div>
+      <div>Word count: 0</div>
+    </div>
+  );
+};
